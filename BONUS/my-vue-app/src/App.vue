e8c149a2ed5c4990f9a660bc4223788522988633
Quintusdev <quintusdev92@gmail.com>
2023-06-19 14:37:24 +0200

    BONUS COMPLETATO

---
+++ BONUS/my-vue-app/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
+
+export default {
+  components: {
+    AppTitle,
+    AppHero
+  }
+}
+</script>
+
+<template>
+  <div>
+    <AppTitle />
+    <hr>
+    <AppHero />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ BONUS/my-vue-app/src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div>
+        <img src="../assets/vue.svg" alt="Jumbotron">
+    </div>
+</template>
+
+<style>
+    
+</style>
